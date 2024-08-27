84626d3ff9a3f348cf2144f97b80f90ec452c9ea
Mr STRATEGIST <142794511+tandavbot2@users.noreply.github.com>
2024-08-27 09:37:00 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -92,8 +92,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('⚡️ ᴍᴏᴠɪᴇs', url='https://t.me/+QVewP06XCPFiYWZl'),
-                    InlineKeyboardButton('🍁 sᴇʀɪᴇs', url='https://t.me/webseries_flix')
+                    InlineKeyboardButton('🚀 ᴅᴇᴠᴇʟᴏᴘᴇʀ', url='https://t.me/TandavBots'),
+                    InlineKeyboardButton('🔧 ꜱᴜᴘᴘᴏʀᴛ', url='https://t.me/TandavBots_Support')
                 ]
             ]
         )
